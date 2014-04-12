b7eeaac8a8c1bde403154fae8951b590cd8cf123
Spiros Raptis <spirosrap@gmail.com>
2014-04-12 16:20:26 +0300

    simple queries

---
+++ 4.55.scm
@@ -0,0 +1,6 @@
+;all people supervised by Ben Bitdiddle;
+(supervisor ?x (Bitdiddle Ben))
+;the names and jobs of all people in the accounting division;
+(job ?x (accounting . ?y))
+;the names and addresses of all people who live in Slumerville.
+(address ?x (Slumerville . ?y))
