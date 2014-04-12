843739eab1567ea0ae6b8b4f80bb2740a64dd94a
Spiros Raptis <spirosrap@gmail.com>
2014-04-12 16:22:20 +0300

    simple queries

--- 4.55.scm
+++ 4.55.scm
@@ -1,3 +1,13 @@
+; SICP exercise 4.55
+;
+; Give simple queries that retrieve the following information from the data
+; base:
+;
+; a. all people supervised by Ben Bitdiddle
+; b. the names and jobs of all people in the accounting division
+; c. the names and addresses of all people who live in Slumerville
+
+
 ;all people supervised by Ben Bitdiddle;
 (supervisor ?x (Bitdiddle Ben))
 ;the names and jobs of all people in the accounting division;
